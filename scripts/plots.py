83bb1fdecdde3fc55e855db8ec3cda69f8d48764
Vitor C. Piro <vitorpiro@gmail.com>
2019-08-16 16:08:51 +0200

    plots

--- scripts/plots.py
+++ scripts/plots.py
@@ -10,6 +10,8 @@ def main():
 	parser.add_argument('-i', metavar='<input_files>', 	required=True, 	dest="input_files", type=str, nargs="*", help="Input files (.npz)")
 	parser.add_argument('-l', metavar='<labels>', 		required=False, dest="labels", 		type=str, nargs="*", default="", help="Labels for the input files")
 	parser.add_argument('-c', metavar='<colors>', 		required=False, dest="colors", 		type=str, nargs="*", default="", help="Colors for the input files")
+	parser.add_argument('-n', metavar='<lines>', 		required=False, dest="lines", 		type=str, nargs="*", default="", help="Lines for the input files")
+	parser.add_argument('-m', metavar='<markers>', 		required=False, dest="markers", 		type=str, nargs="*", default="", help="Markers for the input files")
 	parser.add_argument('-k', metavar='<ranks>', 		required=False, dest="ranks", 		type=str, nargs="*", default="", help="Evaluated ranks. Default: 'superkingdom' 'phylum' 'class' 'order' 'family' 'genus' 'species' 'species+' 'assembly'")
 	parser.add_argument('-o', metavar='<output_plot>', 		required=False, dest="output_plot", type=str, help="Outut figure file")
 	args = parser.parse_args()
@@ -35,18 +37,19 @@ def main():
 
 		tool_name = args.labels[tool_id] if args.labels else input_file.split("/")[-1].split(".")[0]
 		color = args.colors[tool_id] if args.colors else fixed_colors[tool_id]
-		linestyle = "-" 
-		marker = "X"
-		markersize = 12
+		linestyle = args.colors[tool_id] if args.lines else  "-"
+		marker = args.markers[tool_id] if args.markers else "X"
 
-		print(tool_id, tool_name, input_file)
+		markersize = 10
+
+		print(tool_id, tool_name, color, linestyle, marker, input_file)
 		linewidth = 3
 		alpha=0.9
 
-		if 'cs_db' in stats:
-			ax[0].plot([stats['cs_db'][r]/stats['cs_db']['root'] for r in selected_ranks if r in stats['cs_db']], alpha=0.7, linewidth=2, linestyle=linestyle, marker=None, markersize=0, color="black", label="")
-		else:
-			ax[0].plot([stats['db'][r]/stats['db']['root'] for r in selected_ranks if r in stats['db']], alpha=0.7, linewidth=2, linestyle=linestyle, marker=None, markersize=0, color="black", label="")
+		#if 'cs_db' in stats:
+		#	ax[0].plot([stats['cs_db'][r]/stats['cs_db']['root'] for r in selected_ranks if r in stats['cs_db']], alpha=0.7, linewidth=2, linestyle=linestyle, marker=None, markersize=0, color="black", label="")
+		#else:
+		#	ax[0].plot([stats['db'][r]/stats['db']['root'] for r in selected_ranks if r in stats['db']], alpha=0.7, linewidth=2, linestyle=linestyle, marker=None, markersize=0, color="black", label="")
 
 		ax[0].plot([stats['sensitivity'][r] for r in selected_ranks if r in stats['sensitivity'] and stats['sensitivity'][r]], alpha=alpha, linewidth=linewidth, linestyle=linestyle, marker=marker, markersize=markersize, color=color, label=tool_name)
 		# Precision
