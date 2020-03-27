464fc2a958af592f85421cd99fd30f356a9eac42
Vitor C. Piro <vitorpiro@gmail.com>
2020-03-27 10:59:51 +0100

    readme, ganon version and kraken2 on plots

--- scripts/plots.sh
+++ scripts/plots.sh
@@ -9,6 +9,7 @@ clark_marker="o"
 diamond_marker="d"
 ganon_marker="X"
 kraken_marker="s"
+kraken2_marker="p"
 
 evaluations=("cumu" "rank")
 datasets=("challenge" "toy")
@@ -23,16 +24,16 @@ for ev in ${evaluations[@]}; do
        ganon_eval/new_results/20181219_abfv_refseq_cg_t3a/cami_${dt}_H01${suffix}/*.${ev}.npz \
        ganon_eval/new_results/20181219_abfv_refseq_all_t3a/cami_${dt}_H01${suffix}/*.${ev}.npz \
     -k 'phylum' 'class' 'order' 'family' 'genus' 'species' 'species+' \
-    -l "centrifuge OLD" "clark OLD" "diamond OLD" "ganon OLD" "kraken OLD" \
-       "centrifuge CG" "clark CG" "diamond CG" "ganon CG" "kraken CG" \
-       "diamond ALL" "ganon ALL" \
-    -c "${blue_color}" "${blue_color}" "${blue_color}" "${blue_color}" "${blue_color}" \
-       "${orange_color}" "${orange_color}" "${orange_color}" "${orange_color}" "${orange_color}" \
-       "${red_color}" "${red_color}" \
-    -m "${centifuge_marker}" "${clark_marker}" "${diamond_marker}" "${ganon_marker}" "${kraken_marker}" \
-       "${centifuge_marker}" "${clark_marker}" "${diamond_marker}" "${ganon_marker}" "${kraken_marker}" \
-       "${diamond_marker}" "${ganon_marker}" \
-    -o all_${dt}_${ev}${suffix}.png > all_${dt}_${ev}${suffix}.log
+    -l "centrifuge OLD" "clark OLD" "diamond OLD" "ganon OLD" "kraken OLD" "kraken2 OLD" \
+       "centrifuge CG-top-3" "clark CG-top-3" "diamond CG-top-3" "ganon CG-top-3" "kraken CG-top-3" "kraken2 CG-top-3" \
+       "diamond ALL-top-3" "ganon ALL-top-3" "kraken2 ALL-top-3" \
+    -c "${blue_color}" "${blue_color}" "${blue_color}" "${blue_color}" "${blue_color}" "${blue_color}" \
+       "${orange_color}" "${orange_color}" "${orange_color}" "${orange_color}" "${orange_color}" "${orange_color}" \
+       "${red_color}" "${red_color}" "${red_color}" \
+    -m "${centifuge_marker}" "${clark_marker}" "${diamond_marker}" "${ganon_marker}" "${kraken_marker}" "${kraken2_marker}" \
+       "${centifuge_marker}" "${clark_marker}" "${diamond_marker}" "${ganon_marker}" "${kraken_marker}" "${kraken2_marker}" \
+       "${diamond_marker}" "${ganon_marker}" "${kraken2_marker}" \
+    -o all_${dt}_${ev}${suffix}.pdf > all_${dt}_${ev}${suffix}.log
 
   done
 done
